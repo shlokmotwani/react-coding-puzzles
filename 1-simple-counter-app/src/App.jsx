d05175e8a6e4f63ff4731a1339bec28a2049a920
Shlok Motwani <shlok.codes@gmail.com>
2025-01-03 22:59:44 +0530

    feat: increment and decrement buttons

--- 1-simple-counter-app/src/App.jsx
+++ 1-simple-counter-app/src/App.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleIncrement(){
+    setCount(prev => prev + 1);
+  }
+
+  function handleDecrement(){
+    setCount(prev => prev - 1);
+  }
+
+  return(
+    <div id='wrapper-main'>
+      <div>{count}</div>
+      <div>
+        <button onClick={handleIncrement}>+</button>
+        <button onClick={handleDecrement}>-</button>
+      </div>
+    </div>
+  );
 }
 
 export default App
