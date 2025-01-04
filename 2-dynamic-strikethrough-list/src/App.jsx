1c6647149ced265e6c82b8b86de4417cb239f534
Shlok Motwani <shlok.codes@gmail.com>
2025-01-04 17:04:40 +0530

    strikethrough implementation

--- 2-dynamic-strikethrough-list/src/App.jsx
+++ 2-dynamic-strikethrough-list/src/App.jsx
@@ -1,7 +1,37 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { tasks } from "./tasks";
 
 function App() {
- 
+  const [list, setList] = useState(tasks);
+
+  function handleCheckBoxToggle(index) {
+    let arr = list.slice();
+    arr[index].finished = !arr[index].finished;
+    setList(arr);
+  }
+
+  return (
+    <div id="list-wrapper">
+      <ul>
+        {list.map((task, index) => {
+          return (
+            <li key={index}>
+              <div>
+                <input
+                  type="checkbox"
+                  onChange={() => {
+                    handleCheckBoxToggle(index);
+                  }}
+                />
+                {task.finished ? <del>{task.title}</del> : task.title}
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ 2-dynamic-strikethrough-list/src/tasks.js
@@ -0,0 +1,28 @@
+const tasks = [
+  {
+    title: "Get items from the grocery store",
+    finished: false,
+  },
+  {
+    title: "Get the car repaired",
+    finished: false,
+  },
+  {
+    title: "Study Web Development for an hour",
+    finished: false,
+  },
+  {
+    title: "Learn brewing good coffee",
+    finished: false,
+  },
+  {
+    title: "Workout at the gym",
+    finished: false,
+  },
+  {
+    title: "Attend a meeting with the client",
+    finished: false,
+  },
+];
+
+export { tasks };
